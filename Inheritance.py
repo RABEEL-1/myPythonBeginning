cdb25200ca48781c62891f130c0f631c95752be4
Raubeel <siniorchair@gmail.com>
2025-09-24 20:32:14 +0000

    Learning inheritance

---
+++ Inheritance.py
@@ -0,0 +1,26 @@
+class Animal:
+    def __init__(self,name):
+        self.name=name
+        self.isalive=True
+
+    def eat(self):
+         print(f"{self.name} is eating")
+        
+    def sleep(self):
+        print(f"{self.name} is sleeping")
+
+
+class Dog(Animal):
+    def speak(self):
+        print("Woof")
+
+
+class Cat(Animal):
+    pass
+
+dog =Dog("Scooby")
+cat=Cat("grafield")
+
+print(dog.name)
+dog.eat()
+cat.sleep()
